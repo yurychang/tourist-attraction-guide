09168b2d3120ac3f43e36047d55f1a8de4cf9686
peterrabityu <peterrabityu@gmail.com>
2021-11-30 00:10:05 +0800

    add nebular, bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NbThemeModule } from '@nebular/theme';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, NbThemeModule.forRoot(), SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
